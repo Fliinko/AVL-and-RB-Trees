ae69d36d26fc888fa2804a45330cead4131b34b3
Flinkoo <owenagius24@gmail.com>
2021-04-10 18:43:18 +0200

    Created Files, Created Generate Function

---
+++ avl.py
@@ -0,0 +1,21 @@
+import random
+import sys
+
+def generate():
+
+    n = random.randint(1000, 3000)
+    m = random.randint(500, 1000)
+
+    X = set()
+    Y = set()
+
+    for x in range(n):
+        x = random.randint(-3000, 3000)
+        X.add(x)
+
+    for y in range(m):
+        y = random.randint(-3000, 3000)
+        Y.add(y)
+
+    #print("Set X: \n", X)
+    #print("Set Y: \n",  Y)
